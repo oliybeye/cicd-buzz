dfa023852507dd323fd2372e89f1c2af876cf3dd
oliybeye <oliyadbeyene@gmail.com>
2019-03-10 13:05:36 -0700

    issue 1

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 from buzz import generator
 
 app = Flask(__name__)
 
 @app.route("/")
 def generate_buzz():
-    page = '<html><body><h1>'
-    page += generator.generate_buzz()
-    page += '</h1></body></html>'
-    return page
+    buzz = generator.generate_buzz()
+    return render_template('index.html', buzz=buzz)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
