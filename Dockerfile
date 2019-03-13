3a8a002d2e097f9996603e1fba3fdfff9c522875
oliybeye <oliyadbeyene@gmail.com>
2019-03-12 22:15:25 -0700

    deployment config file

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,4 @@ COPY app.py /src
 COPY buzz /src/buzz
 COPY static /src/static
 COPY templates /src/templates
-CMD python /src/app.py
+CMD ["python", "/src/app.py"]
--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import os
+import signal
 from flask import Flask, render_template
 from buzz import generator
 
 app = Flask(__name__)
 
+signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
 @app.route("/")
 def generate_buzz():
     buzz = generator.generate_buzz()
